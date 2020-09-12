70026fde1815ce3b12e28fb8e32cc67b43e70df8
Nelma da Silva Barros <nelmasb@gmail.com>
2020-09-12 12:48:18 -0300

    Aplicando estilo e funcionalidade a busca

--- components/search/Search.js
+++ components/search/Search.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 
 import styles from './Search.module.scss';
 
 class Search extends React.PureComponent {
 
+  static propTypes = {
+    fetcher: PropTypes.func
+  }
+
   render() {
-    return (<p className={styles.Teste}>Search</p>)
+    return (
+      <div className={styles.SearchLogo}>
+        <div className={styles.CustomInput}>
+          <input type="text" placeholder="Nunca deixe de buscar" className={styles.SearchInput} />
+          <i className={styles.SearchIcon} onClick={this.props.fetcher}></i>
+        </div>
+      </div>
+    )
   }
 }
 
